5f112e26d9265f44a9eb0a854354662020b89358
yusufberatsensaaltik-sudo <yusufberatsensaaltik@gmail.com>
2026-07-19 01:15:42 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-// Web sayfasının görüntülenebilmesi için index.html dosyasını sunucuya tanıtıyoruz
+// Web sayfasının görüntülenebilmesi için public.html dosyasını sunucuya tanıtıyoruz
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public.html'));
-    
+});
 
 // Oyuncuların aktif durumlarını tutacak liste
 let oyuncular = {};
 
-// Bir oyuncu oyuna (web sitesine) bağlandığında çalışacak alan
+// Bir oyuncu oyuna bağlandığında çalışacak alan
 io.on('connection', (socket) => {
     console.log('Yeni bir oyuncu bağlandı! ID: ' + socket.id);
 
@@ -23,9 +23,9 @@ io.on('connection', (socket) => {
         io.emit('sistemMesaji', `🌍 Bir oyuncu <b>${secilenUlke}</b> ülkesinin yönetimini devraldı!`);
     });
 
-    // Bir oyuncu hamle yapıp "Turu İlerlet" butonuna bastığında
+    // Bir oyuncu hamle yapıp butonuna bastığında
     socket.on('hamleYap', (data) => {
-        // Yapılan hamleyi ve yazılan özel emri odadaki diğer 2 arkadaşına da anlık gönder
+        // Yapılan hamleyi odadaki diğer arkadaşlarına da anlık gönder
         io.emit('yeniOlay', {
             yapan: oyuncular[socket.id] || "Bilinmeyen Ülke",
             hedef: data.hedef,
@@ -43,8 +43,8 @@ io.on('connection', (socket) => {
     });
 });
 
-// Sunucuyu 3000 portunda başlatıyoruz
-const PORT = 3000;
+// Render sunucusunun otomatik atayacağı portu dinamik olarak dinliyoruz
+const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-    console.log(`Oyun sunucusu hazır! Arkadaşlarınla oynamak için tarayıcıdan http://localhost:${PORT} adresine git.`);
+    console.log(`Sunucu hazır, port: ${PORT}`);
 });
